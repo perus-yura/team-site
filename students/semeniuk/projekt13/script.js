6058fcd278d27997bd9371a9f97af9b48ac783ef
meryx <fili12121294@gmail.com>
2026-05-21 09:58:50 +0300

    зробила практичну 13

--- students/semeniuk/projekt13/script.js
+++ students/semeniuk/projekt13/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded',function() {
-    const parallaxElement = document.querySelectorAll('.parallax-section');
+    const parallaxElements = document.querySelectorAll('.parallax-section');
     window.addEventListener('scroll', function() {
         let scrollPosition =window.pageYOffset;
 
         parallaxElements.forEach(function(element) {
-            let spedd = element.dataset.speed || 0.5;
+            let speed = element.dataset.speed || 0.5;
             element.style.backgroundPositionY = -(scrollPosition * speed) +'px';
         })
     })
